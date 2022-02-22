c74bdf8bff1f7cc2ce251aa3bfa13f08a4347479
Lucas Ribeiro <lucas.ribeiro711@gmail.com>
2022-02-21 23:35:55 -0300

    add script

---
+++ script.js
@@ -0,0 +1,30 @@
+function load() {
+  let msg = document.querySelector('#msg')
+  let img = document.querySelector('#img')
+  let date = new Date()
+  let hours = date.getHours()
+  let period = document.querySelector('#periodMsg')
+  let periodMsg = ''
+  
+  if (hours >7 && hours <= 12) {
+    document.body.style.background = "#0284c7"
+    img.src = 'assets/dia.png'
+    periodMsg = 'Bom dia!'
+  } else if (hours > 12 && hours <= 18) {
+    document.body.style.background = '#d97706'
+    img.src = 'assets/tarde.png'
+    periodMsg = 'Boa tarde!'
+  } else if (hours > 18 && hours <= 0){
+    document.body.style.background = '#111827'
+    img.src = 'assets/noite.png'
+    periodMsg = 'Boa noite!'
+  } else {
+    document.body.style.background = 'red'
+    periodMsg = 'Você não deveria estar dormindo?'
+    img.src = 'assets/madruga.png'
+  }
+
+  msg.innerHTML = `Agora são ${hours} horas`
+  period.innerHTML = periodMsg
+  
+}
