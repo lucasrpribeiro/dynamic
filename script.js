0f50cf6fc9047c17a600793e0197e113500501fe
Lucas Ribeiro <lucas.ribeiro711@gmail.com>
2022-02-26 10:53:26 -0300

    refatoração

--- script.js
+++ script.js
@@ -1,29 +1,49 @@
-function load () {
-  let message = document.querySelector('.msg')
-  let image = document.querySelector('.img')
-  let currentMessage = document.querySelector('#periodMsg')
+function load() {
+  let msg = document.querySelector('.msg')
+  let img = document.querySelector('.img')
+  let currentMsg = document.querySelector('#periodMsg')
   let date = new Date()
   let hour = date.getHours()
 
   if (hour >= 7 && hour < 12) {
+
     document.body.style.background = '#0284c7'
-    currentMessage.innerHTML = 'Tenha um bom dia!'
-    message.innerHTML = `Agora são ${hour} horas`
-    image.src = 'assets/dia.png'
+
+    msg.innerHTML = `Agora são ${hour}:00 horas`
+
+    img.src = 'assets/dia.png'
+
+    currentMsg.innerHTML = 'Tenha um bom dia!'
+
   } else if (hour >= 12 && hour < 18) {
+
     document.body.style.background = '#d97706'
-    message.innerHTML = `Agora são ${hour} horas`
-    currentMessage.innerHTML = 'Tenha uma boa tarde!'
-    image.src = 'assets/tarde.png'
-  } else if (hour >= 18 && hour <= 23) {
+
+    msg.innerHTML = `Agora são ${hour}:00 horas`
+
+    img.src = 'assets/tarde.png'
+
+    currentMsg.innerHTML = 'Tenha uma boa tarde!'
+
+  } else if (hour >= 18 && hour < 23) {
+
     document.body.style.background = '#111827'
-    message.innerHTML = `Agora são ${hour} horas`
-    currentMessage.innerHTML = 'Tenha uma boa noite!'
-    image.src = 'assets/noite.png'
+
+    msg.innerHTML = `Agora são ${hour}:00 horas`
+
+    img.src = 'assets/noite.png'
+
+    currentMsg.innerHTML = 'Tenha uma boa noite!'
+
   } else {
-    document.body.style.background = 'black'
-    message.innerHTML = `Já são ${hour} horas`
-    currentMessage.innerHTML = 'Vai dormir viado!'
-    image.src = 'assets/madruga.png'
+
+    document.body.style.background = '#581c87'
+
+    msg.innerHTML = `Agora são ${hour}:00 horas`
+
+    img.src = 'assets/madruga.png'
+
+    currentMsg.innerHTML = 'Vai dormir!'
+
   }
-} 
+}
