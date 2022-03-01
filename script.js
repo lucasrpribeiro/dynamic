cdbd3c1973b4dcf21e5382ba1938297f58bbfcd9
Lucas Ribeiro <lucas.ribeiro711@gmail.com>
2022-03-01 15:16:22 -0300

    show minuts too

--- script.js
+++ script.js
@@ -1,49 +1,30 @@
-function load() {
+function load () {
   let msg = document.querySelector('.msg')
   let img = document.querySelector('.img')
-  let currentMsg = document.querySelector('#periodMsg')
+  let dayPeriodMsg = document.querySelector('#periodMsg')
   let date = new Date()
   let hour = date.getHours()
-
+  let minuts = date.getMinutes()
+ 
   if (hour >= 7 && hour < 12) {
-
     document.body.style.background = '#0284c7'
-
-    msg.innerHTML = `Agora são ${hour}:00 horas`
-
-    img.src = 'assets/dia.png'
-
-    currentMsg.innerHTML = 'Tenha um bom dia!'
-
+    dayPeriodMsg.innerHTML = 'Tenha um bom dia!'
+    msg.innerHTML = `Agora são ${hour}:${minuts} horas`
+    img.src = "assets/dia.png"
   } else if (hour >= 12 && hour < 18) {
-
     document.body.style.background = '#d97706'
-
-    msg.innerHTML = `Agora são ${hour}:00 horas`
-
-    img.src = 'assets/tarde.png'
-
-    currentMsg.innerHTML = 'Tenha uma boa tarde!'
-
-  } else if (hour >= 18 && hour < 23) {
-
+    dayPeriodMsg.innerHTML = 'Tenha uma boa tarde!'
+    msg.innerHTML = `Agora são ${hour}:${minuts} horas`
+    img.src = "assets/tarde.png"
+  } else if (hour >= 18 && hour <= 23) {
     document.body.style.background = '#111827'
-
-    msg.innerHTML = `Agora são ${hour}:00 horas`
-
-    img.src = 'assets/noite.png'
-
-    currentMsg.innerHTML = 'Tenha uma boa noite!'
-
+    dayPeriodMsg.innerHTML = 'Tenha uma boa noite!'
+    msg.innerHTML = `Agora são ${hour}:${minuts} horas`
+    img.src = "assets/noite.png"
   } else {
-
-    document.body.style.background = '#581c87'
-
-    msg.innerHTML = `Agora são ${hour}:00 horas`
-
-    img.src = 'assets/madruga.png'
-
-    currentMsg.innerHTML = 'Vai dormir!'
-
+    document.body.style.background = '#881337'
+    dayPeriodMsg.innerHTML = 'Vai dormir, safado!'
+    msg.innerHTML = `Já são ${hour}:${minuts} horas!`
+    img.src = "assets/madruga.png"
   }
 }
